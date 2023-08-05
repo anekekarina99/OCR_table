f6eb6b0effb09aef5dabff09b616299d8873e855
Ane <42421075+anekekarina99@users.noreply.github.com>
2023-08-05 14:49:37 +0000

    change global

--- GlobalStyles.js
+++ GlobalStyles.js
@@ -1,9 +1,20 @@
 
+
 /* fonts */
 export const FontFamily = {
+    robotoMedium: "Roboto-Medium",
     robotoRegular: "Roboto-Regular",
+};
+/* font sizes */
+export const FontSize = {
+    size_xl: 20,
 };
 /* Colors */
 export const Color = {
     whiteLight: "#fdfdfd",
+    gray_100: "#141414",
+};
+/* border radiuses */
+export const Border = {
+    br_base: 16,
 };
